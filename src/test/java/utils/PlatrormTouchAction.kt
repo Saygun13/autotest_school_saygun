9ee0da94b3afb687dc917c3a59e99d73f314a58c
Say_gun <dm.saygunov@gmail.com>
2022-01-21 20:27:05 +0300

    adding TouchAction class

---
+++ src/test/java/utils/PlatrormTouchAction.kt
@@ -0,0 +1,7 @@
+package utils
+
+import io.appium.java_client.PerformsTouchActions
+import io.appium.java_client.TouchAction
+
+class PlatformTouchAction(performsTouchActions: PerformsTouchActions) :
+    TouchAction<PlatformTouchAction>(performsTouchActions)
