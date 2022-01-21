bce013f4e4b2d82581a72a0278aeb3e396bd06ee
Say_gun <dm.saygunov@gmail.com>
2022-01-21 20:36:01 +0300

    upgr test methods

--- src/test/java/main/TestMethods.kt
+++ src/test/java/main/TestMethods.kt
@@ -2,14 +2,18 @@ package main
 
 import io.appium.java_client.MobileBy
 import io.appium.java_client.MobileElement
-import main.BaseClass
+import constructor.classes.locatorsTypes
+import io.appium.java_client.touch.WaitOptions
+import io.appium.java_client.touch.offset.PointOption
+import utils.PlatformTouchAction
+import java.time.Duration
 
 open class TestMethods : BaseClass() {
 
     fun clickToElement(locatorType: String, locator: String) {
-
         lateinit var element: MobileElement
         when (locatorType) {
+
             "id" -> element = driver.findElement(MobileBy.id(locator))
             "xpath" -> element = driver.findElement(MobileBy.xpath(locator))
         }
@@ -26,5 +30,26 @@ open class TestMethods : BaseClass() {
         element.sendKeys(inputText)
     }
 
+    fun swipeOnScreen(
+        startCordX: Int,
+        startCordY: Int,
+        moveCordX: Int,
+        moveCordY: Int
+    ) {
+        PlatformTouchAction(driver)
+            .longPress(PointOption.point(startCordX, startCordY))
+            .moveTo(PointOption.point(moveCordX, moveCordY))
+            .release()
+            .perform()
+    }
 
+    fun tapByCoordinates(
+        cordX: Int,
+        cordY: Int,
+    ) {
+        PlatformTouchAction(driver)
+            .tap(PointOption.point(cordX, cordY))
+            .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+            .perform()
+    }
 }
