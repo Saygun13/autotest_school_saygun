d363ee5a9239768b93e8691f7b36649a5ab4adcf
Say_gun <dm.saygunov@gmail.com>
2022-01-23 21:41:53 +0300

    new test methods

--- src/test/java/main/TestMethods.kt
+++ src/test/java/main/TestMethods.kt
@@ -5,6 +5,8 @@ import io.appium.java_client.MobileElement
 import constructor.classes.locatorsTypes
 import io.appium.java_client.touch.WaitOptions
 import io.appium.java_client.touch.offset.PointOption
+import org.testng.AssertJUnit
+import utils.Config
 import utils.PlatformTouchAction
 import java.time.Duration
 
@@ -13,7 +15,6 @@ open class TestMethods : BaseClass() {
     fun clickToElement(locatorType: String, locator: String) {
         lateinit var element: MobileElement
         when (locatorType) {
-
             "id" -> element = driver.findElement(MobileBy.id(locator))
             "xpath" -> element = driver.findElement(MobileBy.xpath(locator))
         }
@@ -28,6 +29,37 @@ open class TestMethods : BaseClass() {
             "xpath" -> element = driver.findElement(MobileBy.xpath(locator))
         }
         element.sendKeys(inputText)
+    }
+
+    // Есть вопрос по поводу проверки доступности. Почему нельзя просто использовать isDisplayed,
+    fun elementIsDisplayed(locatorType: String, locator: String) {
+
+        lateinit var element: MobileElement
+        when (locatorType) {
+            "id" -> element = driver.findElement(MobileBy.id(locator))
+            "xpath" -> element = driver.findElement(MobileBy.xpath(locator))
+        }
+        element.isDisplayed
+    }
+
+    fun checkAvailableElement (locatorType: String, locator: String) {
+        var checkAvailableElement = false // создаем объект типа Boolean
+        when (locatorType) {
+        "id" -> checkAvailableElement = driver.findElement(MobileBy.id(locator)).isEnabled
+        "AccessibilityId" -> checkAvailableElement = driver.findElement(MobileBy.AccessibilityId(locator)).isEnabled
+        "xpath" -> checkAvailableElement = driver.findElement(MobileBy.xpath(locator)).isEnabled
+    } // у поиска сразу происходит обращение к атрибуту и возвращает true или false
+    AssertJUnit.assertTrue(checkAvailableElement) // проверка значение checkAvailableElement на true, возвращает true или false
+}
+
+    fun clearField(locatorType: String, locator: String) {
+
+        lateinit var element: MobileElement
+        when (locatorType) {
+            "id" -> element = driver.findElement(MobileBy.id(locator))
+            "xpath" -> element = driver.findElement(MobileBy.xpath(locator))
+        }
+        element.clear()
     }
 
     fun swipeOnScreen(
